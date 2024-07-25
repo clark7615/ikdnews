5ae1b45c04ef92d290bfa91bd84645ce65bfae74
Clark Ke <clark7615@gmail.com>
2024-07-25 15:56:40 +0800

    fix url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <a href="/ebook/08/index.html">
         <img src="/ebook/08/shot.png" alt="Image description" />
       </a>
-      <a href="/ebook/08/index.html">
+      <a href="/ebook/09/index.html">
         <img src="/ebook/09/shot.png" alt="Image description" />
       </a>
     </div>
