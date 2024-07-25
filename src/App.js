1187e4aef842019ecf1e7c2852b07e1b976c94e9
Clark Ke <clark7615@gmail.com>
2024-07-25 16:20:14 +0800

    fix ui

--- src/App.js
+++ src/App.js
@@ -3,14 +3,51 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <a href="/ebook/08/index.html">
-        <img src="/ebook/08/shot.png" alt="Image description" />
-      </a>
-      <a href="/ebook/09/index.html">
-        <img src="/ebook/09/shot.png" alt="Image description" />
-      </a>
+    <div className="App App-header">
+      <h1 class="embed-h">崇正月刊測試發行版
+      </h1>
+      <div className='mian-m'>
+        <div class="iframe-m">
+          <a href="/ebook/09/index.html">
+            <img src="/ebook/09/shot.png" alt='崇正月刊-九月號' />
+          </a>
+          <h2>9月號</h2>
+        </div>
+        <div class="iframe-m">
+          <a href="/ebook/08/index.html">
+            <img src="/ebook/08/shot.png" alt='崇正月刊-八月號' />
+          </a>
+          <h2>8月號</h2>
+        </div>
+      </div>
+      <div className='mian-m'>
+        <div class="iframe-m">
+          <a href="/ebook/09/index.html">
+            <img src="/ebook/09/shot.png" alt='崇正月刊-九月號' />
+          </a>
+          <h2>9月號</h2>
+        </div>
+        <div class="iframe-m">
+          <a href="/ebook/08/index.html">
+            <img src="/ebook/08/shot.png" alt='崇正月刊-八月號' />
+          </a>
+          <h2>8月號</h2>
+        </div>
+      </div>
+      <div className='mian-m'>
+        <div class="iframe-m">
+          <a href="/ebook/09/index.html">
+            <img src="/ebook/09/shot.png" alt='崇正月刊-九月號' />
+          </a>
+          <h2>9月號</h2>
+        </div>
+        <div class="iframe-m">
+          <a href="/ebook/08/index.html">
+            <img src="/ebook/08/shot.png" alt='崇正月刊-八月號' />
+          </a>
+          <h2>8月號</h2>
+        </div>
+      </div>
     </div>
   );
 }
