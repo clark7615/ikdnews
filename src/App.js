aa4a1e99ec7b6a3b62d99cee0471184bea6e73ce
Clark Ke <clark7615@gmail.com>
2024-07-25 15:54:32 +0800

    cerate react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <a href="/ebook/08/index.html">
+        <img src="/ebook/08/shot.png" alt="Image description" />
+      </a>
+      <a href="/ebook/08/index.html">
+        <img src="/ebook/09/shot.png" alt="Image description" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
